e3a6d3d79a3a2ebad9ef2e4f5135f82e00540297
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-03-26 23:51:19 +0100

    Add download script

---
+++ data/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bash
+
+# TODO: Move data to versioned repository (figshare/zenodo)
+wget -O 2025_02_19_wholeTissue_melanomaAb_01-Scene-2-ScanRegion1big_c1-3.tiff https://datashare.biochem.mpg.de/s/ttQNxgyrLzLZq3f/download
