7ddeb67fda8ee67daa0e5b42a776ebfa1b1f737a
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-07-20 00:20:47 +0200

    Download datasets

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,13 @@
 #!/usr/bash
 
 # TODO: Move data to versioned repository (figshare/zenodo)
+# 5.3 GB
 wget -O 2025_02_19_wholeTissue_melanomaAb_01-Scene-2-ScanRegion1big_c1-3.tiff https://datashare.biochem.mpg.de/s/ttQNxgyrLzLZq3f/download
+
+# TODO: Move data to versioned repository (figshare/zenodo)
+# 4.5 kb
+wget -O metadata_keratinocytes.tsv https://datashare.biochem.mpg.de/s/fQaEeCx2dL2D58r/download
+
+# TODO: Move data to versioned repository (figshare/zenodo)
+# 1.5 mb
+wget -O dvp_proteomics_keratinocytes.tsv https://datashare.biochem.mpg.de/s/fQaEeCx2dL2D58r/download
